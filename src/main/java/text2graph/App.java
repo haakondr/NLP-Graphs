d6b76af614d25fba5a1664aa5b399683cd68c65b
Håkon Drolsum Røkenes <haakondr@gmail.com>
2012-08-20 11:48:37 +0200

    first commit

---
+++ src/main/java/text2graph/App.java
@@ -0,0 +1,13 @@
+package text2graph;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
