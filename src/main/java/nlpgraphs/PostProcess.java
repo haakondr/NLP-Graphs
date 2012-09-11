f0428053d4476d37b3005c4835edc6e783d355a2
Håkon Drolsum Røkenes <haakondr@gmail.com>
2012-09-11 16:00:44 +0200

    ..

--- src/main/java/nlpgraphs/PostProcess.java
+++ src/main/java/nlpgraphs/PostProcess.java
@@ -14,20 +14,28 @@ import nlpgraphs.postprocessing.PlagiarismWorker;
 
 public class PostProcess {
 
-	  public static void main(String[] args) {
-	    	File[] trainFiles = Fileutils.getFiles(Paths.get(args[0]));
-	    	List<Graph> trainGraphs = new ArrayList<>();
-	    	for (File file : trainFiles) {
-				trainGraphs.add(GraphUtils.parseGraph(file.toPath()));
-			}
-	    	File[] test = Fileutils.getFiles(Paths.get(args[1]));
-	    	
-	    	int threads = Runtime.getRuntime().availableProcessors() / test.length;
-	    	PlagiarismWorker worker = new PlagiarismWorker(trainGraphs.toArray(new Graph[0]), Arrays.asList(test), threads, Paths.get(args[0]));
-	   
-	    	ForkJoinPool pool = new ForkJoinPool();
-	    	List<String> results = pool.invoke(worker);
-	    	
-	    	Fileutils.writeToFile("plag.txt", results.toArray(new String[0]));
-	    }
+	public static void main(String[] args) {
+		File[] trainFiles = Fileutils.getFiles(Paths.get(args[0]));
+		List<Graph> trainGraphs = new ArrayList<>();
+		for (File file : trainFiles) {
+			trainGraphs.add(GraphUtils.parseGraph(file.toPath()));
+		}
+		File[] test = Fileutils.getFiles(Paths.get(args[1]));
+
+		int cpuCount = Runtime.getRuntime().availableProcessors() - 2;
+		int threads = cpuCount;
+		if(test.length < cpuCount) {
+			threads = test.length;
+		}else if(cpuCount < 2) {
+			cpuCount = 1;
+		}
+
+		System.out.println("using "+cpuCount+" threads");
+		PlagiarismWorker worker = new PlagiarismWorker(trainGraphs.toArray(new Graph[0]), Arrays.asList(test), threads, Paths.get(args[0]));
+
+		ForkJoinPool pool = new ForkJoinPool();
+		List<String> results = pool.invoke(worker);
+
+		Fileutils.writeToFile("plag.txt", results.toArray(new String[0]));
+	}
 }
