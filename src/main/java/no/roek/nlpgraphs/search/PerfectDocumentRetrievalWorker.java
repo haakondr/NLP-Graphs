11f04ec3032aaa1396674fa6ee9c49ff02e54b98
Haakon <haakon2@haakon-laptop.(none)>
2012-09-25 14:39:06 +0200

    ..

--- src/main/java/no/roek/nlpgraphs/search/PerfectDocumentRetrievalWorker.java
+++ src/main/java/no/roek/nlpgraphs/search/PerfectDocumentRetrievalWorker.java
@@ -1,13 +1,11 @@
 package no.roek.nlpgraphs.search;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
-import com.sun.xml.internal.ws.util.xml.XmlUtil;
 
 import no.roek.nlpgraphs.concurrency.Job;
 import no.roek.nlpgraphs.document.PlagiarismReference;
