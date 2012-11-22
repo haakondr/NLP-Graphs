b9ea8b5b672557d9878336896e07b9ad9a63ecfb
Håkon Drolsum Røkenes <haakondr@gmail.com>
2012-11-22 20:05:49 +0100

    ..

--- src/main/java/no/roek/nlpgraphs/misc/ProgressPrinter.java
+++ src/main/java/no/roek/nlpgraphs/misc/ProgressPrinter.java
@@ -33,7 +33,7 @@ public class ProgressPrinter {
 	    System.out.print("\r" + bar.toString()+" | "+text);
 	}
 	
-	public synchronized boolean isDone() {
+	public boolean isDone() {
 		return current >= total;
 	}
 }
